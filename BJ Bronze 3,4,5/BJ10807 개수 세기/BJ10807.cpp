c61ef8866adff0fd7d4d465bd196b7512a27cc18
sayishuu <smile52673@gmail.com>
2023-01-16 16:23:47 +0900

    개수 세기

---
+++ BJ Bronze 3,4,5/BJ10807 개수 세기/BJ10807.cpp
@@ -0,0 +1,37 @@
+//BJ10807 개수 세기 브5
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    vector<int> Pcount(101);
+    vector<int> Mcount(101);
+    vector<int> v(101);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+        if(v[i]>=0)
+        {
+            Pcount[v[i]]++;
+        }
+        else if(v[i]<0)
+        {
+            Mcount[v[i]*(-1)]++;
+        }
+    }
+
+    int s;
+    cin >> s;
+
+    if(s>=0)
+        cout << Pcount[s];
+    else if(s<0)
+        cout << Mcount[s*(-1)];
+
+    return 0;
+}
