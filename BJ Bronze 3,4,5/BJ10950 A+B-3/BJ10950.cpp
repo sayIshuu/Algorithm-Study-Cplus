c1b3b773f3e386e3dd3160556f2beb04c4c245f5
sayishuu <smile52673@gmail.com>
2023-01-17 14:26:24 +0900

    A+B - 3

---
+++ BJ Bronze 3,4,5/BJ10950 A+B-3/BJ10950.cpp
@@ -0,0 +1,19 @@
+//BJ10950 A+B-3 ºê5
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    
+
+    return 0;
+}
