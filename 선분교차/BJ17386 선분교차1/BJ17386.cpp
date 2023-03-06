978bb2f7b8e1a31b2c7558cb6110748b8c6616dc
sayishuu <smile52673@gmail.com>
2023-03-06 23:33:35 +0900

    코테 끝

---
+++ 선분교차/BJ17386 선분교차1/BJ17386.cpp
@@ -0,0 +1,21 @@
+//BJ17386 선분교차1 골3
+#include <iostream>
+#define l 1000000
+using namespace std;
+
+//어려운점.. 좌표범위가 마이너스 가버리면 배열생성할때 곤란시때루
+// -2 ~ 2까지라면.. 배열크기 5. 인덱스-2 = 실제값
+
+typedef struct dot
+{
+    int rkfh;
+    int tpfh;
+}Dot;
+
+//인덱스 - 백만 = 실제 좌표
+vector<vector<Dot>> v
+
+int main(void)
+{
+    return 0;
+}
