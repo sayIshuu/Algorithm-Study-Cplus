96fd160d3fd32a486a0ec31d704ffbbacc44bac5
sayishuu <smile52673@gmail.com>
2023-04-09 20:45:55 +0900

    소수 구하기

---
+++ 2023스터디/에라토스테네스의 체/BJ1929 소수 구하기/BJ1929.cpp
@@ -0,0 +1,33 @@
+//소수 구하기 실3 23.04.09
+#include <iostream>
+#define MAX 1000001
+using namespace std;
+
+bool arr[MAX] = {true, true, false,};
+int main(void)
+{
+    int m, n;   cin >> m >> n;
+
+    for (int i = 2; i < MAX; i++)
+    {
+        //이미 지워졌다면.
+        if(arr[i])
+            continue;
+        else   //소수는 false 상태
+        {
+            for (int j = i+i; j < MAX; j+=i)
+            {
+                arr[j] = true;
+            }    
+        }
+    }
+
+    for (int i = m; i <= n; i++)
+    {
+        if(!arr[i])
+            cout << i << '\n';
+    }
+    
+
+    return 0;
+}
