2415d18b4326954e5a2d047dd1ed4b9c644cac6e
sayishuu <smile52673@gmail.com>
2023-05-25 10:55:06 +0900

    오큰수

---
+++ 2023스터디/자료구조/BJ17298 오큰수/BJ17298.cpp
@@ -0,0 +1,63 @@
+//BJ17298 오큰수 골4 23-1 알고리즘 마무리문제
+#include <iostream>
+#include <stack>
+using namespace std;
+
+typedef struct node
+{
+    int index;
+    int value;
+}NODE;
+
+
+NODE arr[1000001];
+
+stack<NODE> s;
+
+int ans[1000001];
+
+int main(void)
+{
+    int n; cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i].index = i;
+        cin >> arr[i].value;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if(s.empty()){
+            s.push(arr[i]);
+            continue;
+        }
+
+
+        if(arr[i].value > s.top().value)
+        {
+            ans[s.top().index] = arr[i].value;
+            s.pop();
+            i--;
+            continue;
+        }
+        else{
+            s.push(arr[i]);
+        }
+    }
+    int a = s.size();
+    for (int i = 0; i < a; i++)
+    {
+        ans[s.top().index] = -1;
+        s.pop();
+    }
+    
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << ' ';
+    }
+    
+
+    return 0;
+}
