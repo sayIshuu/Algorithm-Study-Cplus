6bfa2e276e9afe190abb80acd0c3215f60d7a504
sayishuu <smile52673@gmail.com>
2023-07-09 19:29:06 +0900

    수들의 합2

---
+++ 2023스터디/2주차/BJ2003 수들의 합2/BJ2003.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; long long m; cin >> n >> m;
+
+    int arr[n];
+    long long sum[n+1];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum[i+1] = sum[i] + arr[i];
+    }
+
+    int end = 1;
+    int answer = 0;
+
+    for (int start = 0; start < n; start++)
+    {
+        if(end == start)
+            end++;
+        
+        long long ls;
+        if(end <= n)
+            ls = sum[end] - sum[start];
+        else
+            ls = sum[n] - sum[start];
+
+        if(ls > m)
+            continue;
+        else if (ls < m)
+        {
+            if(end >= n)
+                break;
+            end++;
+            start--;
+            continue;
+        }
+        else if (ls == m)
+        {
+            answer++;
+            if(end >= n)
+                break;
+            
+            end++;
+            start--;
+            continue;
+        }
+    }
+    
+    cout << answer;
+    
+    return 0;
+}
