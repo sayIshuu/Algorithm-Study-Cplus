518b7cf27305a56b2a489b58e662f7adbdbb2c04
sayishuu <smile52673@gmail.com>
2023-07-18 13:55:40 +0900

    피보나치수5

---
+++ 2023스터디/DP/BJ10870 피보나치수5/BJ10870.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+long long arr[10001] = {0,1,1,};
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin >> n;
+    if(n == 0 || n == 1 || n == 2)
+    {
+        cout << arr[n];
+        return 0;
+    }
+    for (int i = 3; i < 10001; i++)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+        if(i == n)
+        {
+            cout << arr[i];
+            return 0;
+        }
+    }
+    return 0;
+}
