1b8d82c6b159194a00e42013e61e42b8cecbe1bb
sayishuu <smile52673@gmail.com>
2023-07-28 22:55:53 +0900

    점프왕 쩰리 small

---
+++ 2023스터디/5주차/점프왕 쩰리(small)/BJ16173.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+typedef struct
+{
+    int x; int y;
+}s;
+int n; 
+int map[4][4];
+bool visited[4][4];
+queue<s> q;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cin >> map[i][j];
+    }
+    
+    q.push({0,0});
+    visited[0][0] = true;
+    while(!q.empty())
+    {
+        int x = q.front().x;
+        int y = q.front().y;
+        q.pop();
+        if(map[x][y]==-1)
+        {
+            cout << "HaruHaru";
+            return 0;
+        }
+
+        int nx = x+map[x][y];
+        if(nx < n && !visited[nx][y])
+        {
+            visited[nx][y] = true;
+            q.push({nx,y});
+        }
+        int ny = y+map[x][y];
+        if(ny < n && !visited[x][ny])
+        {
+            visited[x][ny] = true;
+            q.push({x,ny});
+        }
+    }
+
+    cout << "Hing";
+    return 0;
+}
