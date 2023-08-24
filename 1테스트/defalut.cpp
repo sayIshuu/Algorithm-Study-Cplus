9377a16fb37b65ac15385234c72509d8506264cd
sayishuu <smile52673@gmail.com>
2023-08-24 23:58:03 +0900

    디폴트 코드

---
+++ 1테스트/defalut.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void input()
+{
+
+}
+
+void solution()
+{
+
+}
+
+void solve()
+{
+    input();
+    solution();
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
