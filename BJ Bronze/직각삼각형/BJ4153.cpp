3bd33628cfae4684afa7b9e5e14b1e9c8ad8562f
sayishuu <smile52673@gmail.com>
2023-09-26 10:54:43 +0900

    직각삼각형

---
+++ BJ Bronze/직각삼각형/BJ4153.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (true)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a == 0 && b == 0 && c == 0)
+            break;
+
+        if (a * a + b * b == c * c)
+            cout << "right\n";
+        else if (a * a + c * c == b * b)
+            cout << "right\n";
+        else if (b * b + c * c == a * a)
+            cout << "right\n";
+        else
+            cout << "wrong\n";
+    }
+
+    return 0;
+}
