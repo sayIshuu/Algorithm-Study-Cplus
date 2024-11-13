ffabfdd8ae00181fec0ea5f2cdfe3301f1f8c5d9
sayishuu <smile52673@gmail.com>
2024-11-14 01:19:13 +0900

    보석 쇼핑

--- 2024스터디/프로그래머스/보석 쇼핑/보석쇼핑.cpp
+++ 2024스터디/프로그래머스/보석 쇼핑/보석쇼핑.cpp
@@ -1,44 +1,68 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-     vector<int> answer;
      int minLen = 100001;
-     unordered_set<string> gemSet;
-     for (int i = 0; i < gems.size(); i++)
-          gemSet.insert(gems[i]);
-     
-     int numOfGems = gemSet.size();
+     int ansStart = 0;
+     int ansEnd = 0;
+
+     unordered_map<string,int> gemSet;
+     for (auto& gem : gems)
+          gemSet[gem] = 0;
 
      int start = 0;
-     int end = numOfGems -1;
+     int end = 0;
+     gemSet[gems[0]]++;
 
-     while (true)
-     {
+     while (true){
           if(end == gems.size())
                break;
 
-          unordered_set<string> tempSet;
-
-          for (int i = start; i < end; i++)
-               tempSet.insert(gems[i]);
-          
-          if(tempSet.size() == numOfGems)
-          {
-               answer.push_back(start+1);
-               answer.push_back(end+1);
-               break;
+          if(end - start < gemSet.size()-1 && end != gems.size()-1){
+               end++;
+               gemSet[gems[end]]++;
+               continue;
           }
-          end++;
+
+          bool isAll = true;
+          for(auto& iter : gemSet){
+               if(iter.second == 0){
+                    end++;
+                    isAll = false;
+                    if(end == gems.size())
+                         break;
+                    //이미 있는 애를 추가해봤자 isAll은 true가 되지 않아.
+                    while(true){
+                         gemSet[gems[end]]++;
+                         if(end == gems.size()-1)
+                              break;
+                         if(gemSet[gems[end]] == 1)
+                              break;
+                         end++;
+                    }
+                    break;
+               }
+          }
+
+          if(isAll){
+               // gemSet[gems[start]]가 2이상이면 계속 빼줌
+               while(gemSet[gems[start]] > 1 && end - start > gemSet.size()-1){
+                    gemSet[gems[start]]--;
+                    start++;
+               }
+               if(minLen > end-start){
+                    minLen = end-start;
+                    ansStart = start;
+                    ansEnd = end;
+               }
+               gemSet[gems[start]]--;
+               start++;
+          }
      }
-     
-
-
 
      //마지막에 인덱스 +1로 추가
-
-     return answer;
+     return {ansStart+1, ansEnd+1};
 }
