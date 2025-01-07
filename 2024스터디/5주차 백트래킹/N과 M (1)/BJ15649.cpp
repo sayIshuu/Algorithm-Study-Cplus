47b02bb7e3d06ae0ab2f3201bc073c64a7d43dee
sayIshuu <smile52673@gmail.com>
2025-01-08 00:47:19 +0900

    n과m 1

---
+++ 2024스터디/5주차 백트래킹/N과 M (1)/BJ15649.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+void input()
+{
+    cin >> n >> m;
+}
+
+void solution()
+{
+
+}
+
+void solve()
+{
+    input();
+    solution();
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
