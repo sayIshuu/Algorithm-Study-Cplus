ac005d603e7f4bf61fdca85c01523457bbdd9d73
sayIshuu <smile52673@gmail.com>
2025-02-18 20:11:03 +0900

    막대기

--- 2024스터디/27주차 비트마스킹/막대기/BJ1094.cpp
+++ 2024스터디/27주차 비트마스킹/막대기/BJ1094.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    int num; cin >> num;
+    int mak = 64;
+    int sum = 0;
+    int ans = 0;
 
+    while(sum != num){
+        if(sum+mak <= num){
+            sum += mak;
+            ans++;
+        }
+        //나누기 2, 곱하기 2를 효율적으로하는 시프트연산
+        mak >>= 1;
+    }
+
+    cout << ans;
     return 0;
 }
